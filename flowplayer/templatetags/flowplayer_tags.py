08c4e135f822c685a69053f75a4ec038c8273911
Martin Fitzpatrick <martin.fitzpatrick@spenglr.org>
2009-12-22 11:28:24 +0000

    Template tag now accepts and *array* of urls and converts it into a flowplayer playlist. Configuration of flowplayer settings now available via the FLOWPLAYER_CONFIG settings variable, containing a dict of (most) available flowplayer settings. A 'default' entry allows for global settings to be used also. Combined with the CSS this allows for example audio/video display to differ, or different players to be used on different pages of the site with a single keyword change in the calling tag.

--- flowplayer/templatetags/flowplayer_tags.py
+++ flowplayer/templatetags/flowplayer_tags.py
@@ -10,20 +10,20 @@ register = Library()
 class FlowPlayerNode(Node):
     "Renderer class for the flowplayer template tag."
     
-    def __init__(self, media_url, player_class):
+    def __init__(self, media, player_class):
         """
         Constructor.
 
         Parameters:
 
-            file_url
-                Video url.
-            player_url
-                Flowplayer url.
+            media
+                Media file url OR an array of urls
+            player_class
+                Type of player to show (changes css class and config source)
         """
         self.player_class = player_class
-        self.media_url = Variable(media_url)    
-    
+        self.media = Variable(media)
+
         global FLOWPLAYER_ITERATOR
         FLOWPLAYER_ITERATOR += 1
         self.player_id = FLOWPLAYER_ITERATOR
@@ -33,19 +33,37 @@ class FlowPlayerNode(Node):
         else:
             self.player_url = "%sflowplayer/FlowPlayerLight.swf" % (settings.MEDIA_URL)
 
-    def render(self, context):
+        # Import the configuration settings to set on the player output
+        # Configuration is defined in the settings (multiple types of player)
+        if 'default' in settings.FLOWPLAYER_CONFIG:
+            self.player_config = settings.FLOWPLAYER_CONFIG['default']
+        else:
+            self.player_config = dict()
 
+        if player_class in settings.FLOWPLAYER_CONFIG:
+            self.player_config.update(settings.FLOWPLAYER_CONFIG[player_class])
+
+    def render(self, context):
         try:           
-            media_url = self.media_url.resolve(context) 
+            # Try resolve this variable in the template context
+            self.media = self.media.resolve(context) 
         except VariableDoesNotExist:
-            media_url = self.media_url   
+            # Cannot resolve, therefore it's an url
+            self.media_url = self.media
+            self.media_playlist = False
+        else:
+            # Can resolve, push first url into the url variable
+            self.media_url = self.media[0]
+            self.media_playlist = self.media
 
         t = loader.get_template('flowplayer/flowplayer.html')
         code_context = Context(
                             {"player_url": self.player_url,
                              "player_id": self.player_id,
                              "player_class": self.player_class,
-                             "media_url": media_url,
+                             "player_config": self.player_config,
+                             "media_url": self.media_url,
+                             "media_playlist":self.media_playlist
                             }, autoescape=context.autoescape)
         return t.render(code_context)
 
@@ -81,9 +99,9 @@ def do_flowplayer(parser, token):
     else:
         player_class = None
 
-    media_url = args[1]
+    media = args[1]
     
-    return FlowPlayerNode(media_url, player_class)
+    return FlowPlayerNode(media, player_class)
 
 
 # register the tag 
